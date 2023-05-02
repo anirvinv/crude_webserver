3d5b901e7919540b171ca6357797dadd047207b3
Anirvin Vaddiyar <anirvinv@gmail.com>
2023-05-02 16:09:31 -0400

    fixed start.sh to only sleep when its testing the servers

--- start.sh
+++ start.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
+FLAG=false
+
 if [ "$1" = "M" ]; then
     make && ./builds/mthread_server.out
-
 elif [ "$1" = "S" ]; then
     make && ./builds/mthread_server.out
-elif [ "$1" = "TM" ]; then
-    make && ./builds/
 elif [ "$1" = "TM" ]; then
     make
     ./builds/mthread_server.out &
     sleep 1 && ./runtests.sh
+    FLAG=true
 elif [ "$1" = "TS" ]; then
     make
     ./builds/sthread_server.out &
     sleep 1 && ./runtests.sh
+    FLAG=true
 else
     echo "Please check the readme on how to use this script"
 fi
@@ -29,6 +30,8 @@ function handleSIGINT() {
 }
 
 trap 'handleSIGINT' SIGINT
-while true; do
-    sleep 1
-done
+if [ $FLAG = true ]; then
+    while true; do
+        sleep 1
+    done
+fi
